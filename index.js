6058c5c76bcc7046c4ac35f5ffc0b2435ec8557f
Bob Yuwono <bob_yuwono@apps.ipb.ac.id>
2022-04-18 00:15:55 +0700

    add test script
    adding test with mocha and supertest

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ const express= require('express');
 
 
 const app = express();
-const port = 5000;
+// //Idiomatic expression in express to route and respond to a client request
+// app.get('/', (req, res) => {       
+//     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+//                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+// });
 
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {       
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
+// app.get('/', (req, res) => {       
+//     res.send('hello world')      //server responds by sending the index.html file to the client's browser
+//                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+// });
 
-app.listen(port, () => {
-    console.log(`Now listening on port ${port}`);
-}); 
+// app.listen(process.env.PORT || port, () => {
+//     console.log(`Now listening on port ${port}`);
+// }); 
+
+
+//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+    res.send('hello world');
+   });
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000), () =>{
+    console.log(`Now listening on port ${port}`)
+};
+
+module.exports = app;
