b6de6941249f7160e01fe0467d98fd4ba1668f96
Bob Yuwono <bob_yuwono@apps.ipb.ac.id>
2022-08-30 09:13:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-const express= require('express');
+var express = require('express');
+var app = express();
 
+app.get('/',function(req,res) {
+  res.send("Hello World");
+});
 
-const app = express();
-// //Idiomatic expression in express to route and respond to a client request
-// app.get('/', (req, res) => {       
-//     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-//                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-// });
-
-// app.get('/', (req, res) => {       
-//     res.send('hello world')      //server responds by sending the index.html file to the client's browser
-//                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-// });
-
-// app.listen(process.env.PORT || port, () => {
-//     console.log(`Now listening on port ${port}`);
-// }); 
-
-
-//Respond with "hello world" for requests that hit our root "/"
-app.get('/', function (req, res) {
-    res.send('hello world');
-   });
-//listen to port 3000 by default
-app.listen(process.env.PORT || 3000), () =>{
-    console.log(`Now listening on port ${port}`)
-};
-
-module.exports = app;
+app.listen(4000);
